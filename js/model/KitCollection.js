ef6d473fc104642365c570ada1ee57f23693e1b5
Jonathan Olson <olsonsjc@gmail.com>
2013-09-09 18:36:50 -0600

    Fixing drop bounds reference (changed to ES5)

--- js/model/KitCollection.js
+++ js/model/KitCollection.js
@@ -58,7 +58,7 @@ define( function( require ) {
             var box = kitCollection.collectionBoxes[i];
             
             // permissive, so that if the box bounds and molecule bounds intersect, we call it a 'hit'
-            if ( box.getDropBounds().intersects( molecule.getPositionBounds() ) ) {
+            if ( box.dropBounds.intersects( molecule.getPositionBounds() ) ) {
 
               // if our box takes this type of molecule
               if ( box.willAllowMoleculeDrop( molecule ) ) {
