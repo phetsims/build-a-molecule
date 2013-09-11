b4095725fb4945e1b4df6eed8cae71db085d96fe
Jonathan Olson <olsonsjc@gmail.com>
2013-09-10 21:50:31 -0600

    Adding notes about cur files

--- js/Images.js
+++ js/Images.js
@@ -20,6 +20,28 @@ define( function() {
       'scissors-up.png',
       'scissors-closed-up.png',
       
+      /* Notes on .cur files:
+icotool -c -o scissors.cur -X 11 -Y 10 scissors.png
+icotool -c -o scissors-closed.cur -X 13 -Y 7 scissors-closed.png
+icotool -c -o scissors-up.cur -X 10 -Y 11 scissors-up.png
+icotool -c -o scissors-closed-up.cur -X 7 -Y 13 scissors-closed-up.png
+
+icotool -c -o scissors.cur scissors.png
+icotool -c -o scissors-closed.cur scissors-closed.png
+icotool -c -o scissors-up.cur scissors-up.png
+icotool -c -o scissors-closed-up.cur scissors-closed-up.png
+
+icotool --hotspot-x=11 --hotspot-y=10 -c -o scissors.cur scissors.png
+icotool --hotspot-x=13 --hotspot-y=7 -c -o scissors-closed.cur scissors-closed.png
+icotool --hotspot-x=10 --hotspot-y=11 -c -o scissors-up.cur scissors-up.png
+icotool --hotspot-x=7 --hotspot-y=13 -c -o scissors-closed-up.cur scissors-closed-up.png
+
+icotool -c -o scissors.ico scissors.png
+icotool -c -o scissors-closed.ico scissors-closed.png
+icotool -c -o scissors-up.ico scissors-up.png
+icotool -c -o scissors-closed-up.ico scissors-closed-up.png
+      */
+      
       // cursor files (for Internet Explorer)
       'scissors.cur',
       'scissors-closed.cur',
