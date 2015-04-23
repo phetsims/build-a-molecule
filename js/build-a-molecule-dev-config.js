aba9e64d503bb25464ff67dc8445c84a73c901ba
Sam Reid <reids@colorado.edu>
2015-04-23 11:43:41 -0600

    Point to text-2.0.12 and deleted text.js, see https://github.com/phetsims/sherpa/issues/15

--- js/build-a-molecule-dev-config.js
+++ js/build-a-molecule-dev-config.js
@@ -15,7 +15,7 @@ require.config( {
     image: '../../chipper/js/requirejs-plugins/image',
     audio: '../../chipper/js/requirejs-plugins/audio',
     string: '../../chipper/js/requirejs-plugins/string',
-    text: '../../sherpa/text',
+    text: '../../sherpa/text-2.0.12',
 
     // PhET libs, uppercase names to identify them in require.js imports
     AXON: '../../axon/js',
