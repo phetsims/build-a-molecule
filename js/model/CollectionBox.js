9a2b526e79367ee7d963bf8539ffa69830b435ea
samreid <reids@colorado.edu>
2018-01-07 08:19:42 -0700

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/643

--- js/model/CollectionBox.js
+++ js/model/CollectionBox.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2017, University of Colorado Boulder
+// Copyright 2013-2018, University of Colorado Boulder
 
 /**
  * Stores multiple instances of a single type of molecule. Keeps track of quantity, and has a desired capacity.
