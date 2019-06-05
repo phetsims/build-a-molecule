3a1cfa1fbce6cd10718cb68ea3870d1368823b02
denz1994 <denz1994@gmail.com>
2019-06-05 17:51:37 -0400

    ES6 conversion of KitPanel.js https://github.com/phetsims/build-a-molecule/issues/92

--- js/view/KitPanel.js
+++ js/view/KitPanel.js
@@ -6,116 +6,117 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
-  var Color = require( 'SCENERY/util/Color' );
-  var BAMConstants = require( 'BUILD_A_MOLECULE/BAMConstants' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var NextPreviousNavigationNode = require( 'SCENERY_PHET/NextPreviousNavigationNode' );
-  var Node = require( 'SCENERY/nodes/Node' );
-  var Path = require( 'SCENERY/nodes/Path' );
-  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Shape = require( 'KITE/Shape' );
-  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
-  var Text = require( 'SCENERY/nodes/Text' );
-  var TextPushButton = require( 'SUN/buttons/TextPushButton' );
+  const buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
+  const Color = require( 'SCENERY/util/Color' );
+  const BAMConstants = require( 'BUILD_A_MOLECULE/BAMConstants' );
+  const NextPreviousNavigationNode = require( 'SCENERY_PHET/NextPreviousNavigationNode' );
+  const Node = require( 'SCENERY/nodes/Node' );
+  const Path = require( 'SCENERY/nodes/Path' );
+  const PhetFont = require( 'SCENERY_PHET/PhetFont' );
+  const Shape = require( 'KITE/Shape' );
+  const StringUtils = require( 'PHETCOMMON/util/StringUtils' );
+  const Text = require( 'SCENERY/nodes/Text' );
+  const TextPushButton = require( 'SUN/buttons/TextPushButton' );
 
   // strings
-  var kitPatternString = require( 'string!BUILD_A_MOLECULE/kitPattern' );
-  var resetKitString = require( 'string!BUILD_A_MOLECULE/resetKit' );
+  const kitPatternString = require( 'string!BUILD_A_MOLECULE/kitPattern' );
+  const resetKitString = require( 'string!BUILD_A_MOLECULE/resetKit' );
 
-  var kitArrowYOffset = 5; // vertical offset of the kit arrows from the top of the kit
+  const kitArrowYOffset = 5; // vertical offset of the kit arrows from the top of the kit
 
-  /**
-   * @param {KitCollection} kitCollectionModel
-   * @param {Rectangle} availableKitBounds
-   * @constructor
-   */
-  function KitPanel( kitCollectionModel, availableKitBounds ) {
-    Node.call( this, {} );
+  class KitPanel extends Node {
+    /**
+     * @param {KitCollection} kitCollectionModel
+     * @param {Rectangle} availableKitBounds
+     * @constructor
+     */
+    constructor( kitCollectionModel, availableKitBounds ) {
+      super();
 
-    //REVIEW: With layout containers, can we avoid making this assumption?
-    assert && assert( BAMConstants.MODEL_VIEW_TRANSFORM.getMatrix().m11() < 0 ); // we assume this and correct for the inversed Y
+      //REVIEW: With layout containers, can we avoid making this assumption?
+      assert && assert( BAMConstants.MODEL_VIEW_TRANSFORM.getMatrix().m11() < 0 ); // we assume this and correct for the inversed Y
 
-    var kitViewBounds = BAMConstants.MODEL_VIEW_TRANSFORM.modelToViewBounds( availableKitBounds );
+      const kitViewBounds = BAMConstants.MODEL_VIEW_TRANSFORM.modelToViewBounds( availableKitBounds );
 
-    /*---------------------------------------------------------------------------*
-     * background
-     *----------------------------------------------------------------------------*/
+      /*---------------------------------------------------------------------------*
+       * background
+       *----------------------------------------------------------------------------*/
 
-    this.addChild( new Path( Shape.bounds( kitViewBounds ), {
-      fill: BAMConstants.KIT_BACKGROUND,
-      stroke: BAMConstants.KIT_BORDER
-    } ) );
+      this.addChild( new Path( Shape.bounds( kitViewBounds ), {
+        fill: BAMConstants.KIT_BACKGROUND,
+        stroke: BAMConstants.KIT_BORDER
+      } ) );
 
-    /*---------------------------------------------------------------------------*
-     * label and next/previous
-     *----------------------------------------------------------------------------*/
+      /*---------------------------------------------------------------------------*
+       * label and next/previous
+       *----------------------------------------------------------------------------*/
 
-    var labelNode = new Text( '', {
-      font: new PhetFont( {
-        size: 18,
-        weight: 'bold'
-      } )
-    } );
-    kitCollectionModel.currentKitProperty.link( function() {
-      labelNode.text = StringUtils.fillIn( kitPatternString, { number: kitCollectionModel.currentKitIndex + 1 } );
-    } );
+      const labelNode = new Text( '', {
+        font: new PhetFont( {
+          size: 18,
+          weight: 'bold'
+        } )
+      } );
+      kitCollectionModel.currentKitProperty.link( () => {
+        labelNode.text = StringUtils.fillIn( kitPatternString, { number: kitCollectionModel.currentKitIndex + 1 } );
+      } );
 
-    //REVIEW: Replace with the Carousel common type, so we don't have to do so much (and it has a better appearance)
-    var navigationNode = new NextPreviousNavigationNode( labelNode, {
-      arrowColor: BAMConstants.KIT_ARROW_BACKGROUND_ENABLED,
-      arrowStrokeColor: BAMConstants.KIT_ARROW_BORDER_ENABLED,
-      arrowWidth: 17,
-      arrowHeight: 24,
-      next: function() {
-        kitCollectionModel.goToNextKit();
-      },
-      previous: function() {
-        kitCollectionModel.goToPreviousKit();
-      },
-      createTouchAreaShape: function( shape, isPrevious ) {
-        // square touch area
-        var dilatedShape = shape.bounds.dilated( 7 );
-        return Shape.bounds( isPrevious ?
-                             dilatedShape.withMaxX( shape.bounds.right + labelNode.width / 2 ) :
-                             dilatedShape.withMinX( shape.bounds.left - labelNode.width / 2 ) );
-      }
-    } );
-    kitCollectionModel.currentKitProperty.link( function() {
-      navigationNode.hasNextProperty.value = kitCollectionModel.hasNextKit();
-      navigationNode.hasPreviousProperty.value = kitCollectionModel.hasPreviousKit();
-    } );
-    navigationNode.setTranslation( kitViewBounds.maxX - navigationNode.width - 5, kitViewBounds.minY + kitArrowYOffset );
-    this.addChild( navigationNode );
+      //REVIEW: Replace with the Carousel common type, so we don't have to do so much (and it has a better appearance)
+      const navigationNode = new NextPreviousNavigationNode( labelNode, {
+        arrowColor: BAMConstants.KIT_ARROW_BACKGROUND_ENABLED,
+        arrowStrokeColor: BAMConstants.KIT_ARROW_BORDER_ENABLED,
+        arrowWidth: 17,
+        arrowHeight: 24,
+        next: () => {
+          kitCollectionModel.goToNextKit();
+        },
+        previous: () => {
+          kitCollectionModel.goToPreviousKit();
+        },
+        createTouchAreaShape: ( shape, isPrevious ) => {
+          // square touch area
+          const dilatedShape = shape.bounds.dilated( 7 );
+          return Shape.bounds( isPrevious ?
+                               dilatedShape.withMaxX( shape.bounds.right + labelNode.width / 2 ) :
+                               dilatedShape.withMinX( shape.bounds.left - labelNode.width / 2 ) );
+        }
+      } );
+      kitCollectionModel.currentKitProperty.link( () => {
+        navigationNode.hasNextProperty.value = kitCollectionModel.hasNextKit();
+        navigationNode.hasPreviousProperty.value = kitCollectionModel.hasPreviousKit();
+      } );
+      navigationNode.setTranslation( kitViewBounds.maxX - navigationNode.width - 5, kitViewBounds.minY + kitArrowYOffset );
+      this.addChild( navigationNode );
 
-    /*---------------------------------------------------------------------------*
-     * refill kit
-     *----------------------------------------------------------------------------*/
+      /*---------------------------------------------------------------------------*
+       * refill kit
+       *----------------------------------------------------------------------------*/
 
-    var refillButton = new TextPushButton( resetKitString, {
-      listener: function() {
-        kitCollectionModel.currentKitProperty.value.reset();
-      },
-      baseColor: Color.ORANGE,
-      font: new PhetFont( { size: 12, weight: 'bold' } ),
-      x: kitViewBounds.minX + 5,
-      y: kitViewBounds.minY + 5
-    } );
-    refillButton.touchArea = Shape.bounds( refillButton.selfBounds.union( refillButton.childBounds ).dilated( 10 ) );
-    var updateRefillButton = function() {
-      refillButton.enabled = kitCollectionModel.currentKitProperty.value.hasAtomsOutsideOfBuckets();
-    };
-    kitCollectionModel.kits.forEach( function( kit ) {
-      kit.addedMoleculeEmitter.addListener( updateRefillButton );
-      kit.removedMoleculeEmitter.addListener( updateRefillButton );
-    } );
-    kitCollectionModel.currentKitProperty.link( updateRefillButton );
-    this.addChild( refillButton );
+      const refillButton = new TextPushButton( resetKitString, {
+        listener: () => {
+          kitCollectionModel.currentKitProperty.value.reset();
+        },
+        baseColor: Color.ORANGE,
+        font: new PhetFont( { size: 12, weight: 'bold' } ),
+        x: kitViewBounds.minX + 5,
+        y: kitViewBounds.minY + 5
+      } );
+      refillButton.touchArea = Shape.bounds( refillButton.selfBounds.union( refillButton.childBounds ).dilated( 10 ) );
+      const updateRefillButton = () => {
+        refillButton.enabled = kitCollectionModel.currentKitProperty.value.hasAtomsOutsideOfBuckets();
+      };
+      kitCollectionModel.kits.forEach( ( kit ) => {
+        kit.addedMoleculeEmitter.addListener( updateRefillButton );
+        kit.removedMoleculeEmitter.addListener( updateRefillButton );
+      } );
+      kitCollectionModel.currentKitProperty.link( updateRefillButton );
+      this.addChild( refillButton );
+    }
   }
-  buildAMolecule.register( 'KitPanel', KitPanel );
 
-  return inherit( Node, KitPanel );
+  return buildAMolecule.register( 'KitPanel', KitPanel );
+
 } );
