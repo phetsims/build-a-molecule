41b3e0c4281313bd6c90ddb9b56d7fd48152058a
denz1994 <denz1994@gmail.com>
2019-06-05 17:46:52 -0400

    ES6 conversion for KitCollectionNode.js https://github.com/phetsims/build-a-molecule/issues/92

--- js/view/KitCollectionNode.js
+++ js/view/KitCollectionNode.js
@@ -6,46 +6,43 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
-  var inherit = require( 'PHET_CORE/inherit' );
-  var KitPanel = require( 'BUILD_A_MOLECULE/view/KitPanel' );
-  var KitView = require( 'BUILD_A_MOLECULE/view/KitView' );
-  var Node = require( 'SCENERY/nodes/Node' );
+  const buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
+  const KitPanel = require( 'BUILD_A_MOLECULE/view/KitPanel' );
+  const KitView = require( 'BUILD_A_MOLECULE/view/KitView' );
+  const Node = require( 'SCENERY/nodes/Node' );
 
-  /**
-   * @param {KitCollectionList} kitCollectionList
-   * @param {KitCollection} collection
-   * @param {MoleculeCollectingView} view
-   * @constructor
-   */
-  function KitCollectionNode( kitCollectionList, collection, view ) {
-    Node.call( this, {} );
-    var self = this;
+  class KitCollectionNode extends Node {
+    /**
+     * @param {KitCollectionList} kitCollectionList
+     * @param {KitCollection} collection
+     * @param {MoleculeCollectingView} view
+     * @constructor
+     */
+    constructor( kitCollectionList, collection, view ) {
+      super( {} );
+      const self = this;
 
-    this.addChild( new KitPanel( collection, kitCollectionList.availableKitBounds ) );
+      this.addChild( new KitPanel( collection, kitCollectionList.availableKitBounds ) );
 
-    var kitMap = {}; // maps kit ID => KitView
-    collection.kits.forEach( function( kit ) {
-      var kitView = new KitView( kit, view );
-      kitMap[ kit.id ] = kitView;
-    } );
+      const kitMap = {}; // maps kit ID => KitView
+      collection.kits.forEach( ( kit ) => {
+        kitMap[ kit.id ] = new KitView( kit, view );
+      } );
 
-    // NOTE: appends to the KitCollectionNode. This works because the KitPanel is always behind (we have a shallower tree this way)
-    collection.currentKitProperty.link( function( newKit, oldKit ) {
-      if ( oldKit ) {
-        self.removeChild( kitMap[ oldKit.id ] );
-      }
-      if ( newKit ) {
-        self.addChild( kitMap[ newKit.id ] );
-      }
-    } );
+      // NOTE: appends to the KitCollectionNode. This works because the KitPanel is always behind (we have a shallower tree this way)
+      collection.currentKitProperty.link( ( newKit, oldKit ) => {
+        if ( oldKit ) {
+          self.removeChild( kitMap[ oldKit.id ] );
+        }
+        if ( newKit ) {
+          self.addChild( kitMap[ newKit.id ] );
+        }
+      } );
+    }
   }
-  buildAMolecule.register( 'KitCollectionNode', KitCollectionNode );
 
-  inherit( Node, KitCollectionNode );
-
-  return KitCollectionNode;
+  return buildAMolecule.register( 'KitCollectionNode', KitCollectionNode );
 } );
