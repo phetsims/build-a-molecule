32f7049a267af40cc697762af1bec0a99ce9af12
denz1994 <denz1994@gmail.com>
2019-08-05 09:14:56 -0400

    ES6 conversion. https://github.com/phetsims/build-a-molecule/issues/92

--- js/BAMConstants.js
+++ js/BAMConstants.js
@@ -1,33 +1,33 @@
 // Copyright 2019, University of Colorado Boulder
 
-define( function( require ) {
+define( require => {
   'use strict';
 
   // modules
-  var buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
-  var Color = require( 'SCENERY/util/Color' );
-  var Dimension2 = require( 'DOT/Dimension2' );
-  var Element = require( 'NITROGLYCERIN/Element' );
-  var ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
-  var ScreenView = require( 'JOIST/ScreenView' );
-  var Util = require( 'DOT/Util' );
-  var Vector2 = require( 'DOT/Vector2' );
+  const buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
+  const Color = require( 'SCENERY/util/Color' );
+  const Dimension2 = require( 'DOT/Dimension2' );
+  const Element = require( 'NITROGLYCERIN/Element' );
+  const ModelViewTransform2 = require( 'PHETCOMMON/view/ModelViewTransform2' );
+  const ScreenView = require( 'JOIST/ScreenView' );
+  const Util = require( 'DOT/Util' );
+  const Vector2 = require( 'DOT/Vector2' );
 
   // Constants
-  var VIEW_SIZE = ScreenView.DEFAULT_LAYOUT_BOUNDS;
-  var MODEL_VIEW_TRANSFORM = ModelViewTransform2.createSinglePointScaleInvertedYMapping(
+  const VIEW_SIZE = ScreenView.DEFAULT_LAYOUT_BOUNDS;
+  const MODEL_VIEW_TRANSFORM = ModelViewTransform2.createSinglePointScaleInvertedYMapping(
     Vector2.ZERO,
     new Vector2( Util.roundSymmetric( VIEW_SIZE.width * 0.5 ), Util.roundSymmetric( VIEW_SIZE.height * 0.5 ) ),
     0.27 * 1.2 // "Zoom factor" - smaller zooms out, larger zooms in
   );
-  var MODEL_SIZE = new Dimension2(
+  const MODEL_SIZE = new Dimension2(
     MODEL_VIEW_TRANSFORM.viewToModelDeltaX( VIEW_SIZE.width ),
     Math.abs( MODEL_VIEW_TRANSFORM.viewToModelDeltaY( VIEW_SIZE.height ) )
   );
-  var VIEW_PADDING = 18;
-  var MODEL_PADDING = MODEL_VIEW_TRANSFORM.viewToModelDeltaX( VIEW_PADDING );
+  const VIEW_PADDING = 18;
+  const MODEL_PADDING = MODEL_VIEW_TRANSFORM.viewToModelDeltaX( VIEW_PADDING );
 
-  var BAMConstants = {
+  const BAMConstants = {
 
 
     /*---------------------------------------------------------------------------*
@@ -74,7 +74,6 @@ define( function( require ) {
       Element.B, Element.Br, Element.C, Element.Cl, Element.F, Element.H, Element.I, Element.N, Element.O, Element.P, Element.S, Element.Si
     ]
   };
-  buildAMolecule.register( 'BAMConstants', BAMConstants );
 
-  return BAMConstants;
+  return buildAMolecule.register( 'BAMConstants', BAMConstants );
 } );
