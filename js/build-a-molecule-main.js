91f139d148205b2f22de0a2924821856171390d8
denz1994 <denz1994@gmail.com>
2019-08-05 09:17:28 -0400

    ES6 conversion. https://github.com/phetsims/build-a-molecule/issues/92

--- js/build-a-molecule-main.js
+++ js/build-a-molecule-main.js
@@ -4,17 +4,17 @@
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
-define( function( require ) {
+define( require => {
   'use strict';
 
-  var buildAMoleculeTitleString = require( 'string!BUILD_A_MOLECULE/build-a-molecule.title' );
-  var CollectMultipleScreen = require( 'BUILD_A_MOLECULE/screens/CollectMultipleScreen' );
-  var LargerMoleculesScreen = require( 'BUILD_A_MOLECULE/screens/LargerMoleculesScreen' );
-  var MakeMoleculeScreen = require( 'BUILD_A_MOLECULE/screens/MakeMoleculeScreen' );
-  var Sim = require( 'JOIST/Sim' );
-  var SimLauncher = require( 'JOIST/SimLauncher' );
+  const buildAMoleculeTitleString = require( 'string!BUILD_A_MOLECULE/build-a-molecule.title' );
+  const CollectMultipleScreen = require( 'BUILD_A_MOLECULE/screens/CollectMultipleScreen' );
+  const LargerMoleculesScreen = require( 'BUILD_A_MOLECULE/screens/LargerMoleculesScreen' );
+  const MakeMoleculeScreen = require( 'BUILD_A_MOLECULE/screens/MakeMoleculeScreen' );
+  const Sim = require( 'JOIST/Sim' );
+  const SimLauncher = require( 'JOIST/SimLauncher' );
 
-  var simOptions = {
+  const simOptions = {
     credits: {
       //TODO (without scrolling credits, the BAM team refuses to take credit!)'
       //REVIEW: Take a first pass at credits
@@ -22,7 +22,7 @@ define( function( require ) {
   };
 
   // if the flag is set on window, don't launch the sim
-    SimLauncher.launch( function() {
+  SimLauncher.launch( () => {
       //Create and start the sim
       new Sim( buildAMoleculeTitleString, [
         new MakeMoleculeScreen(),
