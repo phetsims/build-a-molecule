2904c4cfc25353a7c7cb6f2f8bb2252836af04be
denz1994 <denz1994@gmail.com>
2019-10-30 15:34:35 -0400

    Support for switching between viewStyles in molecule view. Ball and stick still needs work. BondMest matrix is being set with rotation and translation elements.

--- js/view/view3d/Molecule3DDialog.js
+++ js/view/view3d/Molecule3DDialog.js
@@ -14,16 +14,19 @@ define( require => {
   const AquaRadioButton = require( 'SUN/AquaRadioButton' );
   const Bounds2 = require( 'DOT/Bounds2' );
   const buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
+  const Color = require( 'SCENERY/util/Color' );
   const Dialog = require( 'SUN/Dialog' );
   const Enumeration = require( 'PHET_CORE/Enumeration' );
   const EnumerationProperty = require( 'AXON/EnumerationProperty' );
   const inherit = require( 'PHET_CORE/inherit' );
+  const Matrix3 = require( 'DOT/Matrix3' );
   const PhetFont = require( 'SCENERY_PHET/PhetFont' );
   const RichText = require( 'SCENERY/nodes/RichText' );
   const ThreeNode = require( 'MOBIUS/ThreeNode' );
   const Text = require( 'SCENERY/nodes/Text' );
   const HBox = require( 'SCENERY/nodes/HBox' );
   const VBox = require( 'SCENERY/nodes/VBox' );
+  const Vector3 = require( 'DOT/Vector3' );
 
   // strings
   const ballAndStickString = require( 'string!BUILD_A_MOLECULE/ballAndStick' ); // eslint-disable-line string-require-statement-match
@@ -48,8 +51,8 @@ define( require => {
     } );
 
     // Used for radio buttons
-    const viewStyle = new Enumeration( [ 'SPACE_FILL', 'BALL_AND_STICK' ] );
-    const viewStyleProperty = new EnumerationProperty( viewStyle, viewStyle.SPACE_FILL );
+    const ViewStyle = new Enumeration( [ 'SPACE_FILL', 'BALL_AND_STICK' ] );
+    const viewStyleProperty = new EnumerationProperty( ViewStyle, ViewStyle.SPACE_FILL );
 
     // Chemical formula label
     const formulaText = new RichText( completeMolecule.getGeneralFormulaFragment(), {
@@ -73,22 +76,72 @@ define( require => {
       xSpacing: 8,
       stroke: 'white'
     };
-    const spaceFillButton = new AquaRadioButton( viewStyleProperty, viewStyle.SPACE_FILL, spaceFillText, radioButtonOptions );
-    const ballAndStickButton = new AquaRadioButton( viewStyleProperty, viewStyle.BALL_AND_STICK, ballAndStickText, radioButtonOptions );
-
+    const spaceFillButton = new AquaRadioButton( viewStyleProperty, ViewStyle.SPACE_FILL, spaceFillText, radioButtonOptions );
+    const ballAndStickButton = new AquaRadioButton( viewStyleProperty, ViewStyle.BALL_AND_STICK, ballAndStickText, radioButtonOptions );
     const buttonHolder = new HBox( {
       children: [ spaceFillButton, ballAndStickButton ],
       spacing: optionsHorizontalPadding
     } );
 
     // @private 3D view of moleculeNode using mobuis supported elements
-    this.moleculeNode = new ThreeNode( 70, 70 );
+    this.moleculeNode = new ThreeNode( 400, 300, {
+      cameraPosition: new Vector3( 0, 0, 10 )
+    } );
+    this.moleculeContainer = new THREE.Object3D();
     const moleculeScene = this.moleculeNode.stage.threeScene;
+    moleculeScene.add( this.moleculeContainer );
 
-    moleculeScene.add( new THREE.Mesh(
-      new THREE.SphereGeometry( 4, 30, 24 ), new THREE.MeshLambertMaterial( {
-        color: 0xffaa44
-      } ) ) );
+    viewStyleProperty.link( viewStyle => {
+      while ( this.moleculeContainer.children.length > 0 ) {
+        this.moleculeContainer.remove( this.moleculeContainer.children[ 0 ] );
+      }
+      if ( viewStyle === ViewStyle.SPACE_FILL ) {
+        completeMolecule.atoms.forEach( atom => {
+          const atomMesh = new THREE.Mesh( new THREE.SphereGeometry( atom.covalentRadius / 80, 30, 24 ), new THREE.MeshLambertMaterial( {
+            color: Color.toColor( atom.element.color ).toNumber()
+          } ) );
+          const mesh = atomMesh;
+          moleculeScene.add( mesh );
+          mesh.position.set( atom.x3d, atom.y3d, atom.z3d );
+        } );
+      }
+      else {
+        completeMolecule.atoms.forEach( atom => {
+          const atomMesh = new THREE.Mesh( new THREE.SphereGeometry( atom.covalentRadius / 100, 30, 24 ), new THREE.MeshLambertMaterial( {
+            color: Color.toColor( atom.element.color ).toNumber()
+          } ) );
+          this.moleculeContainer.add( atomMesh );
+          atomMesh.position.set( atom.x3d, atom.y3d, atom.z3d );
+        } );
+        completeMolecule.bonds.forEach( ( bond ) => {
+          const bondMesh = new THREE.Mesh( new THREE.CylinderGeometry( .75, .75, 0.25, 32, false ), new THREE.MeshLambertMaterial( {
+            color: Color.gray
+          } ) );
+
+          // Vector3
+          const cylinderDefaultOrientation = Vector3.X_UNIT;
+          let bondAPosition = new Vector3( bond.a.x3d, bond.a.y3d, bond.a.z3d );
+          let bondBPosition = new Vector3( bond.b.x3d, bond.b.y3d, bond.b.z3d );
+          const neededOrientation = bondAPosition.minus( bondBPosition ).normalized();
+
+          // Matrix3
+          const matrix = Matrix3.rotateAToB( cylinderDefaultOrientation, neededOrientation );
+
+          // Vector3
+          const midpointBetweenAtoms = bondAPosition.average( bondBPosition );
+          this.moleculeContainer.add( bondMesh );
+
+          bondMesh.matrix.set(
+            matrix.m00(), matrix.m01(), matrix.m02(), midpointBetweenAtoms.x,
+            matrix.m10(), matrix.m11(), matrix.m12(), midpointBetweenAtoms.y,
+            matrix.m20(), matrix.m21(), matrix.m22(), midpointBetweenAtoms.z,
+            0, 0, 0, 1
+          );
+          bondMesh.position.set( midpointBetweenAtoms.x, midpointBetweenAtoms.y, midpointBetweenAtoms.z );
+        } );
+      }
+    } );
+
 
     // Lights taken from MoleculeShapesScreenView.js
     const ambientLight = new THREE.AmbientLight( 0x191919 ); // closest to 0.1 like the original shader
