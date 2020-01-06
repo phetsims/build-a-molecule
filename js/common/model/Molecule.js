8e394d92df4b8086cb02f19952fe303c464dc01b
denz1994 <denz1994@gmail.com>
2020-01-06 16:23:38 -0500

    ES6 conversion for Molecule.js https://github.com/phetsims/build-a-molecule/issues/92

--- js/common/model/Molecule.js
+++ js/common/model/Molecule.js
@@ -12,50 +12,52 @@ define( require => {
   // modules
   const Bounds2 = require( 'DOT/Bounds2' );
   const buildAMolecule = require( 'BUILD_A_MOLECULE/buildAMolecule' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const MoleculeStructure = require( 'BUILD_A_MOLECULE/common/model/MoleculeStructure' );
 
-  //REVIEW: Do we really want default parameters here?
-  /**
-   * @param numAtoms
-   * @param numBonds
-   * @constructor
-   */
-  function Molecule( numAtoms, numBonds ) {
-    MoleculeStructure.call( this, numAtoms || 0, numBonds || 0 );
-  }
-  buildAMolecule.register( 'Molecule', Molecule );
+  class Molecule extends MoleculeStructure {
+    //REVIEW: Do we really want default parameters here?
+    /**
+     * @param numAtoms
+     * @param numBonds
+     * @constructor
+     */
+    constructor( numAtoms, numBonds ) {
+      super( numAtoms || 0, numBonds || 0 );
+    }
 
-  inherit( MoleculeStructure, Molecule, {
     // Where the molecule is right now
     get positionBounds() {
       // mutable way of handling this, so we need to make a copy
       const bounds = Bounds2.NOTHING.copy();
-      this.atoms.forEach( function( atom ) {
+      this.atoms.forEach( atom => {
         bounds.includeBounds( atom.positionBounds );
       } );
       return bounds;
-    },
+    }
 
     // Where the molecule will end up
     get destinationBounds() {
       // mutable way of handling this, so we need to make a copy
       const bounds = Bounds2.NOTHING.copy();
-      this.atoms.forEach( function( atom ) {
+      this.atoms.forEach( atom => {
         bounds.includeBounds( atom.destinationBounds );
       } );
       return bounds;
-    },
+    }
 
-    // @param {Vector2}
-    shiftDestination: function( delta ) {
-      this.atoms.forEach( function( atom ) {
+    /**
+     * @param {Vector2} delta
+     * @public
+     */
+    shiftDestination( delta ) {
+      this.atoms.forEach( atom => {
         // TODO: memory: consider alternate mutable form atom.destination.add( delta )
         atom.isSeparatingProperty.value = true;
         atom.destinationProperty.value = atom.destinationProperty.value.plus( delta );
       } );
     }
-  } );
+  }
 
-  return Molecule;
+
+  return buildAMolecule.register( 'Molecule', Molecule );
 } );
