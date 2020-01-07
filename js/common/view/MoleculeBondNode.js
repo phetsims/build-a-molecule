afbb203f1a7cf511baa9cd6dec51cc85825ed427
denz1994 <denz1994@gmail.com>
2020-01-07 12:37:01 -0500

    ES6 conversion and doc updates and removed unused code for MoleculeBondNode. https://github.com/phetsims/build-a-molecule/issues/92

--- js/common/view/MoleculeBondNode.js
+++ js/common/view/MoleculeBondNode.js
@@ -15,7 +15,6 @@ define( require => {
   const ButtonListener = require( 'SCENERY/input/ButtonListener' );
   const Circle = require( 'SCENERY/nodes/Circle' );
   const Color = require( 'SCENERY/util/Color' );
-  const inherit = require( 'PHET_CORE/inherit' );
   const Node = require( 'SCENERY/nodes/Node' );
   const platform = require( 'PHET_CORE/platform' );
 
@@ -51,110 +50,106 @@ define( require => {
     'scissors-up.cur': upCursorImage,
     'scissors-closed-up.cur': closedUpCursorImage
   };
-
   const bondRadius = 5; // "Radius" of the bond target that will break the bond
 
-  /**
-   * @param {Bond} bond
-   * @param {Kit} kit
-   * @constructor
-   */
-  function MoleculeBondNode( bond, kit ) {
-    const self = this;
+  class MoleculeBondNode extends Node {
+    /**
+     * @param {Bond} bond
+     * @param {Kit} kit
+     * @constructor
+     */
+    constructor( bond, kit ) {
+      super( {} );
 
-    Node.call( this, {} );
+      // @private
+      this.a = bond.a;
+      this.b = bond.b;
 
-    // @private
-    this.a = bond.a;
-    this.b = bond.b;
-    this.selectedAtom = null;
+      // use the lewis dot model to get our bond direction
+      const bondDirection = kit.getBondDirection( this.a, this.b );
+      const isHorizontal = bondDirection.id.name === 'WEST' || bondDirection.id.name === 'EAST';
 
-    // use the lewis dot model to get our bond direction
-    const bondDirection = kit.getBondDirection( this.a, this.b );
-    const isHorizontal = bondDirection.id.name === 'WEST' || bondDirection.id.name === 'EAST';
-
-    let openFile = 'scissors';
-    let closedFile = 'scissors-closed';
-    if ( isHorizontal ) {
-      openFile += '-up';
-      closedFile += '-up';
-    }
-    if ( platform.ie || platform.edge ) {
-      openFile += '.cur';
-      closedFile += '.cur';
-    }
-    else {
-      openFile += '.png';
-      closedFile += '.png';
-    }
-
-    const scissorsOpen = images[ openFile ]; // 23x20 or 20x23
-    const scissorsClosed = images[ closedFile ]; //26x15 or 15x26
-    const backup = ( isHorizontal ? 'col-resize' : 'row-resize' );
-
-    // offsets should center this
-    let openCursor;
-    let closedCursor;
-    if ( platform.ie || platform.edge ) {
-      openCursor = 'url(' + scissorsOpen.src + '), url(http://phetsims.github.io/build-a-molecule/images/' + openFile + '), ' + backup;
-      closedCursor = 'url(' + scissorsClosed.src + '), url(http://phetsims.github.io/build-a-molecule/images/' + closedFile + '), ' + backup;
-    }
-    else {
-      openCursor = 'url(' + scissorsOpen.src + ') ' + ( isHorizontal ? '10 11' : '11 10' ) + ', ' + backup;
-      closedCursor = 'url(' + scissorsClosed.src + ') ' + ( isHorizontal ? '7 13' : '13 7' ) + ', ' + backup;
-    }
-
-    // Cut here icon is subject to change. See https://github.com/phetsims/build-a-molecule/issues/113
-    const cutTargetNode = new Node();
-    cutTargetNode.addChild( new Circle( bondRadius, {
-      fill: new Color( 'rgb(253,225,49)' ),
-      stroke: new Color( 'rgb(253,225,49)' ),
-      cursor: openCursor,
-      visible: true
-    } ) );
-    cutTargetNode.addInputListener( new ButtonListener( {
-      fire: function() {
-      },
-      up: function() {
-        cutTargetNode.cursor = openCursor;
-      },
-      down: function() {
-        cutTargetNode.cursor = closedCursor;
-        kit.breakBond( self.a, self.b );
+      let openFile = 'scissors';
+      let closedFile = 'scissors-closed';
+      if ( isHorizontal ) {
+        openFile += '-up';
+        closedFile += '-up';
       }
-    } ) );
-    this.addChild( cutTargetNode );
-
-    // Show the cut targets for the selected atom's bonds
-    kit.selectedAtomProperty.link( selectedAtom => {
-      cutTargetNode.visible = selectedAtom === self.a || selectedAtom === self.b;
-    } );
-
-    // listener that will update the position of our hit target
-    this.positionListener = function() {
-      const orientation = self.b.positionProperty.value.minus( self.a.positionProperty.value );
-      if ( orientation.magnitude > 0 ) {
-        orientation.normalize();
+      if ( platform.ie || platform.edge ) {
+        openFile += '.cur';
+        closedFile += '.cur';
       }
-      const location = orientation.times( self.a.covalentRadius ).plus( self.a.positionProperty.value );
-      self.setTranslation( BAMConstants.MODEL_VIEW_TRANSFORM.modelToViewPosition( location ) );
-    };
-    this.a.positionProperty.link( this.positionListener );
-    this.b.positionProperty.link( this.positionListener );
-  }
+      else {
+        openFile += '.png';
+        closedFile += '.png';
+      }
 
-  buildAMolecule.register( 'MoleculeBondNode', MoleculeBondNode );
+      const scissorsOpen = images[ openFile ]; // 23x20 or 20x23
+      const scissorsClosed = images[ closedFile ]; //26x15 or 15x26
+      const backup = ( isHorizontal ? 'col-resize' : 'row-resize' );
 
-  inherit( Node, MoleculeBondNode, {
+      // offsets should center this
+      let openCursor;
+      let closedCursor;
+      if ( platform.ie || platform.edge ) {
+        openCursor = 'url(' + scissorsOpen.src + '), url(http://phetsims.github.io/build-a-molecule/images/' + openFile + '), ' + backup;
+        closedCursor = 'url(' + scissorsClosed.src + '), url(http://phetsims.github.io/build-a-molecule/images/' + closedFile + '), ' + backup;
+      }
+      else {
+        openCursor = 'url(' + scissorsOpen.src + ') ' + ( isHorizontal ? '10 11' : '11 10' ) + ', ' + backup;
+        closedCursor = 'url(' + scissorsClosed.src + ') ' + ( isHorizontal ? '7 13' : '13 7' ) + ', ' + backup;
+      }
+
+      // Cut here icon is subject to change. See https://github.com/phetsims/build-a-molecule/issues/113
+      const cutTargetNode = new Node();
+      cutTargetNode.addChild( new Circle( bondRadius, {
+        fill: new Color( 'rgb(253,225,49)' ),
+        stroke: new Color( 'rgb(253,225,49)' ),
+        cursor: openCursor,
+        visible: true
+      } ) );
+      cutTargetNode.addInputListener( new ButtonListener( {
+        fire() {
+        },
+        up() {
+          cutTargetNode.cursor = openCursor;
+        },
+        down() {
+          cutTargetNode.cursor = closedCursor;
+          kit.breakBond( this.a, this.b );
+        }
+      } ) );
+      this.addChild( cutTargetNode );
+
+      // Show the cut targets for the selected atom's bonds
+      kit.selectedAtomProperty.link( selectedAtom => {
+        cutTargetNode.visible = selectedAtom === this.a || selectedAtom === this.b;
+      } );
+
+      // listener that will update the position of our hit target
+      this.positionListener = () => {
+        const orientation = this.b.positionProperty.value.minus( this.a.positionProperty.value );
+        if ( orientation.magnitude > 0 ) {
+          orientation.normalize();
+        }
+        const location = orientation.times( this.a.covalentRadius ).plus( this.a.positionProperty.value );
+        this.setTranslation( BAMConstants.MODEL_VIEW_TRANSFORM.modelToViewPosition( location ) );
+      };
+      this.a.positionProperty.link( this.positionListener );
+      this.b.positionProperty.link( this.positionListener );
+    }
+
     /**
      * @override
+     * @public
      */
-    dispose: function() {
+    dispose() {
       this.a.positionProperty.unlink( this.positionListener );
       this.b.positionProperty.unlink( this.positionListener );
       Node.prototype.dispose.call( this );
     }
-  } );
+  }
 
-  return MoleculeBondNode;
+
+  return buildAMolecule.register( 'MoleculeBondNode', MoleculeBondNode );
 } );
