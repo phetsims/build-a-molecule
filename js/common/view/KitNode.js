df834c48a0807394147859a066af60d5cbdc1470
denz1994 <denz1994@gmail.com>
2020-06-17 10:26:44 -0400

    Remove check for clo

--- js/common/view/KitNode.js
+++ js/common/view/KitNode.js
@@ -113,7 +113,13 @@ class KitNode extends Node {
             // if it's not in our bucket, ignore it (could skip weird cases where an atom outside of the bucket is
             // technically closer)
             if ( !bucket.particleList.contains( atom ) ) {
-              atom = bucket.particleList.getArray()[ 0 ];
+              const bucketAtoms = bucket.particleList.getArray();
+              if ( bucketAtoms.length ) {
+                atom = bucketAtoms[ 0 ];
+              }
+              else {
+                return;
+              }
             }
             atomNodeDragCallback( event, atom );
           }
