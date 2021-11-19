41b4336b1b15f8d0d994a5ddb2b51e8fed0ea27b
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:45 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/buildAMoleculeStrings.ts
@@ -0,0 +1,74 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import buildAMolecule from './buildAMolecule.js';
+
+type StringsType = {
+  'acetylene': string,
+  'ammonia': string,
+  'borane': string,
+  'carbonDioxide': string,
+  'carbonMonoxide': string,
+  'chloromethane': string,
+  'ethylene': string,
+  'fluoromethane': string,
+  'formaldehyde': string,
+  'hydrogenCyanide': string,
+  'hydrogenPeroxide': string,
+  'hydrogenSulfide': string,
+  'methane': string,
+  'molecularChlorine': string,
+  'molecularFluorine': string,
+  'molecularHydrogen': string,
+  'molecularNitrogen': string,
+  'molecularOxygen': string,
+  'nitricOxide': string,
+  'nitrousOxide': string,
+  'ozone': string,
+  'phosphine': string,
+  'silane': string,
+  'sulfurDioxide': string,
+  'trifluoroborane': string,
+  'water': string,
+  'fluorine': string,
+  'iodine': string,
+  'collectionMultipleGoalPattern': string,
+  'chlorine': string,
+  'nitrogen': string,
+  'collectionMultipleQuantityPattern': string,
+  'nextCollection': string,
+  'silicon': string,
+  'bromine': string,
+  'moleculeNamePattern': string,
+  'collectionSinglePattern': string,
+  'title': {
+    'playground': string,
+    'single': string,
+    'multiple': string
+  },
+  'yourMolecules': string,
+  'threeD': string,
+  'phosphorus': string,
+  'boron': string,
+  'oxygen': string,
+  'collectionPattern': string,
+  'hydrogen': string,
+  'sulphur': string,
+  'collectionMultipleQuantityEmpty': string,
+  'build-a-molecule': {
+    'title': string
+  },
+  'youCompletedYourCollection': string,
+  'warning': string,
+  'carbon': string
+};
+
+const buildAMoleculeStrings = getStringModule( 'BUILD_A_MOLECULE' ) as StringsType;
+
+buildAMolecule.register( 'buildAMoleculeStrings', buildAMoleculeStrings );
+
+export default buildAMoleculeStrings;
