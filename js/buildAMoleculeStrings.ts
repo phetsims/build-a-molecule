f49e45befaed7d7fe6dd7928b170c6faa965f01d
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:59 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/buildAMoleculeStrings.ts
+++ js/buildAMoleculeStrings.ts
@@ -8,63 +8,63 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import buildAMolecule from './buildAMolecule.js';
 
 type StringsType = {
-  'acetylene': string,
-  'ammonia': string,
-  'borane': string,
-  'carbonDioxide': string,
-  'carbonMonoxide': string,
-  'chloromethane': string,
-  'ethylene': string,
-  'fluoromethane': string,
-  'formaldehyde': string,
-  'hydrogenCyanide': string,
-  'hydrogenPeroxide': string,
-  'hydrogenSulfide': string,
-  'methane': string,
-  'molecularChlorine': string,
-  'molecularFluorine': string,
-  'molecularHydrogen': string,
-  'molecularNitrogen': string,
-  'molecularOxygen': string,
-  'nitricOxide': string,
-  'nitrousOxide': string,
-  'ozone': string,
-  'phosphine': string,
-  'silane': string,
-  'sulfurDioxide': string,
-  'trifluoroborane': string,
-  'water': string,
-  'fluorine': string,
-  'iodine': string,
-  'collectionMultipleGoalPattern': string,
-  'chlorine': string,
-  'nitrogen': string,
-  'collectionMultipleQuantityPattern': string,
-  'nextCollection': string,
-  'silicon': string,
-  'bromine': string,
-  'moleculeNamePattern': string,
-  'collectionSinglePattern': string,
+  'acetylene': string;
+  'ammonia': string;
+  'borane': string;
+  'carbonDioxide': string;
+  'carbonMonoxide': string;
+  'chloromethane': string;
+  'ethylene': string;
+  'fluoromethane': string;
+  'formaldehyde': string;
+  'hydrogenCyanide': string;
+  'hydrogenPeroxide': string;
+  'hydrogenSulfide': string;
+  'methane': string;
+  'molecularChlorine': string;
+  'molecularFluorine': string;
+  'molecularHydrogen': string;
+  'molecularNitrogen': string;
+  'molecularOxygen': string;
+  'nitricOxide': string;
+  'nitrousOxide': string;
+  'ozone': string;
+  'phosphine': string;
+  'silane': string;
+  'sulfurDioxide': string;
+  'trifluoroborane': string;
+  'water': string;
+  'fluorine': string;
+  'iodine': string;
+  'collectionMultipleGoalPattern': string;
+  'chlorine': string;
+  'nitrogen': string;
+  'collectionMultipleQuantityPattern': string;
+  'nextCollection': string;
+  'silicon': string;
+  'bromine': string;
+  'moleculeNamePattern': string;
+  'collectionSinglePattern': string;
   'title': {
-    'playground': string,
-    'single': string,
-    'multiple': string
-  },
-  'yourMolecules': string,
-  'threeD': string,
-  'phosphorus': string,
-  'boron': string,
-  'oxygen': string,
-  'collectionPattern': string,
-  'hydrogen': string,
-  'sulphur': string,
-  'collectionMultipleQuantityEmpty': string,
+    'playground': string;
+    'single': string;
+    'multiple': string;
+  };
+  'yourMolecules': string;
+  'threeD': string;
+  'phosphorus': string;
+  'boron': string;
+  'oxygen': string;
+  'collectionPattern': string;
+  'hydrogen': string;
+  'sulphur': string;
+  'collectionMultipleQuantityEmpty': string;
   'build-a-molecule': {
-    'title': string
-  },
-  'youCompletedYourCollection': string,
-  'warning': string,
-  'carbon': string
+    'title': string;
+  };
+  'youCompletedYourCollection': string;
+  'warning': string;
+  'carbon': string;
 };
 
 const buildAMoleculeStrings = getStringModule( 'BUILD_A_MOLECULE' ) as StringsType;
