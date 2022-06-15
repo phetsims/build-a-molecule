2fc94552a61cdb8d02c4bbc275b2f3bf2658baa9
phet-dev <phetadmin@gmail.com>
2022-06-15 03:26:29 -0600

    update copyright dates from daily grunt work

--- js/common/model/MoleculeStructure.js
+++ js/common/model/MoleculeStructure.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2021, University of Colorado Boulder
+// Copyright 2020-2022, University of Colorado Boulder
 
 /**
  * Represents a general molecular structure (without position or instance information).
