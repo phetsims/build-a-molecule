ab8bb665055e1889b830c2a31df37f50bc510380
phet-dev <phetadmin@gmail.com>
2023-02-04 03:58:07 -0700

    update copyright dates from daily grunt work

--- js/common/view/KitPanel.js
+++ js/common/view/KitPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * Contains the kit background and controls for switching between kits
