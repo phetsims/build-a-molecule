e5dcac924dcf93f6f420185953ff41a89836debf
phet-dev <phetadmin@gmail.com>
2023-03-09 03:59:40 -0700

    update copyright dates from daily grunt work

--- js/common/view/WarningDialog.js
+++ js/common/view/WarningDialog.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2022, University of Colorado Boulder
+// Copyright 2020-2023, University of Colorado Boulder
 
 /**
  * Dialog that displays a warning text. Used as a webgl fallback when webgl isn't supported.
