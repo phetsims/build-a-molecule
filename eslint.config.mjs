43c9672622405aa255108da0d6071410ad570ce2
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:58 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for build-a-molecule.
+ * ESLint configuration for build-a-molecule.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
