6b87253592572b7ff86ecd84908bf802faf74a72
phet-dev <phetadmin@gmail.com>
2025-08-01 03:15:23 -0600

    update copyright dates from daily grunt work

--- js/common/model/BAMModel.js
+++ js/common/model/BAMModel.js
@@ -1,4 +1,4 @@
-// Copyright 2020-2024, University of Colorado Boulder
+// Copyright 2020-2025, University of Colorado Boulder
 
 /**
  * An internal list of collections that a user will be able to scroll through using a control on the collection area
