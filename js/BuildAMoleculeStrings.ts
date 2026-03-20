a2f9f46bef7b3e6120594516c06c79722f77892d
phet-dev <phetadmin@gmail.com>
2026-03-20 13:48:11 -0600

    grunt update  from daily grunt work

--- js/BuildAMoleculeStrings.ts
+++ js/BuildAMoleculeStrings.ts
@@ -126,4 +126,6 @@ type StringsType = {
 
 const BuildAMoleculeStrings = getStringModule( 'BUILD_A_MOLECULE' ) as StringsType;
 
+buildAMolecule.register( 'BuildAMoleculeStrings', BuildAMoleculeStrings );
+
 export default BuildAMoleculeStrings;
